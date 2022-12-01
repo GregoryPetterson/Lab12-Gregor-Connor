f63b380dd3aa8328f5c41a9f72146b5238deda3b
Gregory Peterson <pet03356@morris.umn.edu>
2022-12-01 14:35:12 -0600

    Write the base code for BreakString.java

---
+++ src/BreakString.java
@@ -0,0 +1,17 @@
+
+public class BreakString {
+    public String string;
+    public int numberOfBreaks;
+    public int[] breakPoints;
+
+    BreakString(String input, int[] breakPoints){
+        this.string = input;
+        this.numberOfBreaks = breakPoints.length;
+        this.breakPoints = breakPoints;
+    }
+
+    
+
+
+    
+}
