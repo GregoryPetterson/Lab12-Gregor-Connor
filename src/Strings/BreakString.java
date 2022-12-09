dea1b1e20dd0e5381354ce30a80e5ab3db91a43b
Kahnurd <hetti031@morris.umn.edu>
2022-12-08 20:44:23 -0600

    Make it so it outputs best value for each subproblem
    At the moment it seems like it completely solves a given problem and outputs the best value for each subproblem.
    The only current issue is I don't know why it finds the subproblem cutting a string of length 4 at position 2, since the problem I give it doesn't feel like that would ever happen.
    Next step is to add the order in which each cut happens.

--- src/Strings/BreakString.java
+++ src/Strings/BreakString.java
@@ -8,17 +8,21 @@ import java.util.List;
 
 public class BreakString {
     Hashtable<String, Integer> memo = new Hashtable<String, Integer>();
+    
     public static void main(String[] args) {
         // int len = Integer.parseInt(args[0]);
         // int breakPoints = Integer.parseInt(args[1]);
         BreakString potato = new BreakString();
 
-        int len = 20;
-        int numberOfBreaks = 3;
+        int len = 30;
+        int numberOfBreaks = 6;
         List<Integer> breaks = new ArrayList<Integer>();
         breaks.add(2);
         breaks.add(8);
-        breaks.add(10);     
+        breaks.add(10);  
+        breaks.add(20); 
+        breaks.add(26); 
+        breaks.add(28); 
         System.out.println(potato.breakStringsMemo(len, numberOfBreaks, breaks));
         System.out.println(potato.memo);
 
@@ -33,7 +37,7 @@ public class BreakString {
 
     public static int breakStrings(int n, int numberofBreaks, List<Integer> listOfBreaks)
     {
-        if(n == 0 || numberofBreaks == 0) return 0;
+        if(n <= 0 || numberofBreaks == 0) return 0;
 
         int q = Integer.MAX_VALUE;
         for (int i = 0; i < numberofBreaks; i++) {
@@ -58,9 +62,9 @@ public class BreakString {
 
     public int breakStringsMemo(int n, int numberofBreaks, List<Integer> listOfBreaks)
     {
-        if(n == 0 || numberofBreaks == 0) return 0;
+        if(n <= 0 || numberofBreaks == 0) return 0;
         if(memo.get(n + listOfBreaks.toString()) != null){
-            System.out.println("Skipping subtree..." + n + listOfBreaks.toString());
+            //System.out.println("Skipping subtree..." + n + listOfBreaks.toString());
             return memo.get(n + listOfBreaks.toString());
 
 
@@ -69,17 +73,25 @@ public class BreakString {
         for (int i = 0; i < numberofBreaks; i++) {    
                  
             int currentBreak = listOfBreaks.get(i);
-            List<Integer> p2 = listOfBreaks.subList(0, i);
-            List<Integer> p3 = listOfBreaks.subList(i+1, numberofBreaks);
+            List<Integer> p2 = new ArrayList<>(listOfBreaks.subList(0, i));
+            List<Integer> p3 =  new ArrayList<>(listOfBreaks.subList(i+1, numberofBreaks));
+            for (int j = 0; j < p3.size(); j++) {
+                p3.set(j, p3.get(j) - currentBreak);
+            }
+            
             
             q = Math.min(q , n + breakStringsMemo(currentBreak, p2.size(), p2) + breakStringsMemo(n - currentBreak, p3.size(), p3));
-            System.out.println("String Length : " + n);
-            System.out.println("Cutting at: " + currentBreak);
-            System.out.println("Two halves: " + currentBreak + " and " + (n - currentBreak));
-            System.out.println("Resulting Value: " + q);
-            System.out.println("-------------------------------------------------------------------");
+            
+            // System.out.println("String Length : " + n);
+            // System.out.println("Current Cuts : " + listOfBreaks);
+            // System.out.println("Cutting at: " + currentBreak);
+            // System.out.println("Two halves: " + currentBreak + " and " + (n - currentBreak));
+            // System.out.println("Two halves: " + p2 + " and " + p3);
+            // System.out.println("Resulting Value: " + q);
+            // System.out.println("-------------------------------------------------------------------");
         }
         this.memo.put(n + listOfBreaks.toString(), q);
+        System.out.println("Best outcome for: " + n + " " + listOfBreaks + "---> " + q);
         return q;
     }
 
