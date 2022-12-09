56c433f63b26e627c5a874c83743164b2b2a52ad
Kahnurd <hetti031@morris.umn.edu>
2022-12-08 19:25:22 -0600

    Make a function that uses a Hashtable to increase efficiency
    It isn't entirely clear that it uses the Hashtable effectively yet, but it still solves the example problem.
    Also Introducing a new class called 'pair' that will eventually hold the q value plus the order of cuts made to get that value.

--- src/Strings/BreakString.java
+++ src/Strings/BreakString.java
@@ -1,11 +1,17 @@
+package Strings;
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.List;
 
 public class BreakString {
+    Hashtable<String, Integer> memo = new Hashtable<String, Integer>();
     public static void main(String[] args) {
         // int len = Integer.parseInt(args[0]);
         // int breakPoints = Integer.parseInt(args[1]);
+        BreakString potato = new BreakString();
 
         int len = 20;
         int numberOfBreaks = 3;
@@ -13,7 +19,8 @@ public class BreakString {
         breaks.add(2);
         breaks.add(8);
         breaks.add(10);     
-        System.out.println(BreakString.breakStrings(len, numberOfBreaks, breaks));
+        System.out.println(potato.breakStringsMemo(len, numberOfBreaks, breaks));
+        System.out.println(potato.memo);
 
         int[] p = {0, 1, 5, 8, 9, 12};
 
@@ -26,8 +33,8 @@ public class BreakString {
 
     public static int breakStrings(int n, int numberofBreaks, List<Integer> listOfBreaks)
     {
-        if(n == 0) return 0;
-        if(numberofBreaks == 0) return 0;
+        if(n == 0 || numberofBreaks == 0) return 0;
+
         int q = Integer.MAX_VALUE;
         for (int i = 0; i < numberofBreaks; i++) {
             System.out.println("i: " + i);
@@ -44,7 +51,35 @@ public class BreakString {
             System.out.println(p3);
             System.out.println("-------------------------------------------------------------------");
             q = Math.min(q , n + breakStrings(currentBreak, p2.size(), p2) + breakStrings(n - currentBreak, p3.size(), p3));
+            
         }
+        return q;
+    }
+
+    public int breakStringsMemo(int n, int numberofBreaks, List<Integer> listOfBreaks)
+    {
+        if(n == 0 || numberofBreaks == 0) return 0;
+        if(memo.get(n + listOfBreaks.toString()) != null){
+            System.out.println("Skipping subtree..." + n + listOfBreaks.toString());
+            return memo.get(n + listOfBreaks.toString());
+
+
+        } 
+        int q = Integer.MAX_VALUE;
+        for (int i = 0; i < numberofBreaks; i++) {    
+                 
+            int currentBreak = listOfBreaks.get(i);
+            List<Integer> p2 = listOfBreaks.subList(0, i);
+            List<Integer> p3 = listOfBreaks.subList(i+1, numberofBreaks);
+            
+            q = Math.min(q , n + breakStringsMemo(currentBreak, p2.size(), p2) + breakStringsMemo(n - currentBreak, p3.size(), p3));
+            System.out.println("String Length : " + n);
+            System.out.println("Cutting at: " + currentBreak);
+            System.out.println("Two halves: " + currentBreak + " and " + (n - currentBreak));
+            System.out.println("Resulting Value: " + q);
+            System.out.println("-------------------------------------------------------------------");
+        }
+        this.memo.put(n + listOfBreaks.toString(), q);
         return q;
     }
 
---
+++ src/Strings/Pair.java
@@ -0,0 +1,12 @@
+package Strings;
+public class Pair {
+    int value;
+    int[] orderOfBreaks;
+
+    Pair(int value, int[] orderOfBreaks){
+        this.value = value;
+        this.orderOfBreaks = orderOfBreaks;
+    }
+
+    
+}
